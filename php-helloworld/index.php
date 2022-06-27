9f8f8f6603a9cc293024c33e7692df2239662ca5
= <student@workstation.lab.example.com>
2022-06-27 00:12:52 -0400

    print hostname

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,5 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
-print "A change is a coming!\n";
-print "less tahn 3 hours from exam\n";
+print "hostname is :", gethostname();
 ?>
